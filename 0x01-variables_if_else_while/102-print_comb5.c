ba474407e277301cc626eea3242477223787a57b
ruffbam <138764163+ruffbam@users.noreply.github.com>
2023-08-11 21:42:18 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,72 +1,30 @@
-Toggle navigationMenu 
-H
-alx-low_level_programming
-Project information
-Repository
-Files
-Commits
-Branches
-Tags
-Contributor statistics
-Graph
-Compare revisions
-Issues
-1
-Merge requests
-0
-CI/CD
-Deployments
-Packages and registries
-Monitor
-Analytics
-Wiki
-Snippets
-
-Close sidebar
-Martin Smith
-alx-low_level_programming
-Repository
-alx-low_level_programming
-0x01-variables_if_else_while
-102-print_comb5.c
-102-print_comb5.c
-551 B
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Succes)
  */
+
 int main(void)
 {
-	int tho;
-	int hun;
-	int ten;
-	int bas;
-for ( tho = 0 ; tho < 10 ; tho++)
-{	
-	for (hun = 0 ; hun <= 10 ; hun++)
+	int n;
+	int i;
+
+	for (n = 0 ; n < 9 ; n++)
 	{
-	
-		for (ten = 0 ; ten < 10 ; ten++)
-		{
-			for ( bas = 0 ; bas < 10 ; bas++)
+		i = n + 1;
+		do {
+			putchar('0' + n);
+			putchar('0' + i);
+			if (n < 8)
 			{
-				putchar('0' + tho);
-				putchar('0' + hun);
+				putchar(',');
 				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-				bas++;
 			}
-		}
+			i++;
+		} while (i < 10);
 	}
-}
 	putchar('\n');
 	return (0);
-
+}
