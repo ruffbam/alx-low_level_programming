2038c819ca04a1c31b6e3ab32b3a79eaea2a64e8
ruffbam <138764163+ruffbam@users.noreply.github.com>
2023-08-12 10:35:26 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@
 
 int main(void)
 {
-     int num1, num2;
+int num1, num2;
 
-    for (num1 = 0; num1 <= 98; num1++)
+for (num1 = 0; num1 <= 98; num1++)
         {
              for (num2 = num1 + 1; num2 <= 99; num2++)
               {
