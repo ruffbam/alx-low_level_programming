bf465d88344e6ea36075a0876ac632843b645d2b
ruffbam <138764163+ruffbam@users.noreply.github.com>
2023-08-18 13:23:59 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,15 +7,15 @@
 void times_table(void)
 {
 	int v, h;
-	
+
 	for (h = 0 ; h < 10 ; h++)
 	{
 		_putchar('0');
 		for (v = 1 ; v < 10 ; v++)
 		{
-			
-			int sum = v * h;
-			_putchar(',');
+	               int sum = v * h;
+	
+	        	_putchar(',');
 			if (sum / 100 == 0)
 			{
 				_putchar(32);
