fa03bbc53aec94b246533202c6ec40f6778a7132
ruffbam <138764163+ruffbam@users.noreply.github.com>
2023-08-18 13:10:45 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,34 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * main - check the code
+ * jack_bauer - print every minute of the day, starting from 00:00 - 23:59.
  *
- * Return: Always 0.
+ * Return : void
  */
-int main(void)
+void jack_bauer(void)
 {
-    jack_bauer();
-    return (0);
+	int h0, h1;
+	int m0, m1;
+
+	for (h0 = 0 ; h0 <= 2 ; h0++)
+	{
+		for (h1 = 0 ; h1 < 10 ; h1++)
+		{
+			for (m0 = 0 ; m0 < 6 ; m0++)
+			{
+				for (m1 = 0 ; m1 < 10 ; m1++)
+				{
+					_putchar('0' + h0);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m0);
+					_putchar('0' + m1);
+					_putchar('\n');
+					if (h0 == 2 && h1 == 3 && m0 == 5 && m1 == 9)
+					{
+						return;
+					}
+				}
+			}
+		}
+	}
 }
