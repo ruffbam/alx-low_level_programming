98f1539284571ff2236e9b0168fcb61b287aaa65
ruffbam <138764163+ruffbam@users.noreply.github.com>
2023-08-19 17:09:52 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,21 +1,4 @@
 /**
- * print_alphabt - print the lower-case alphabet on one line.
- */
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(char c);
-int _isalpha(char c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-
-/**
  * prototypes for module 0x03
  */
 int _isupper(int c);
@@ -29,4 +12,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
