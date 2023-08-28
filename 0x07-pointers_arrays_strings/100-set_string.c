eae080b346cfd3dbf2a5f1a987ec684e6f23c5b3
Yusuf Rufai Bamidele <ruffbamreal@gmail.com>
2023-08-28 17:43:10 +0100

    yes 	new file:   100-set_string.c 	new file:   101-crackme_password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - function that sets the value of a pointer to a char.
+ * @s: source for copy
+ * @to: string to copy to.
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
